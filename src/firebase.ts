7e3c6214dcd89e3680f33070ff7684e5ab5d2f1e
Planting12 <simej1214@gmail.com>
2023-11-15 22:28:59 +0900

    ✨ feat: firebase 초기화

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBq-amFvN__jeZmvOTsO5aGqPcEO6LB3vk",
+  authDomain: "sns-project-2d357.firebaseapp.com",
+  projectId: "sns-project-2d357",
+  storageBucket: "sns-project-2d357.appspot.com",
+  messagingSenderId: "650771449616",
+  appId: "1:650771449616:web:fac7cf50d92b6073c64795",
+};
+
+const app = initializeApp(firebaseConfig);
