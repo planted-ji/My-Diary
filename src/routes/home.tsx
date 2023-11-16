16637954e184ce4ec99bd718183feb0e499d349f
Planting12 <simej1214@gmail.com>
2023-11-16 17:07:39 +0900

    ✨ feat: Home 화면에 recordForm 컴포넌트 추가

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,12 @@
-import { auth } from "../firebase";
+import { styled } from "styled-components";
+import RecordForm from "../components/record-form";
+
+const Wrapper = styled.div``;
 
 export default function Home() {
-  const logOut = () => {
-    auth.signOut();
-  };
   return (
-    <h1>
-      <button onClick={logOut}>Log Out</button>
-    </h1>
+    <Wrapper>
+      <RecordForm />
+    </Wrapper>
   );
 }
