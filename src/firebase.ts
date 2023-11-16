31f7edb918dc18e0dfe06b500fac0f6e7aeb3e7c
Planting12 <simej1214@gmail.com>
2023-11-16 16:10:10 +0900

    ✨ feat: Storage와 Database에 대한 액세스 권한 추가

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBq-amFvN__jeZmvOTsO5aGqPcEO6LB3vk",
@@ -12,4 +14,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+// Firebase와 연결된 인증 객체 가져오기
 export const auth = getAuth(app);
+
+// Storage와 Database에 대한 액세스 권한
+export const storage = getStorage(app);
+export const db = getFirestore(app);
