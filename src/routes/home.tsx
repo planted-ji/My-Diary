6136b03b1e9da377ae186dd5fbb8a72adb1bf910
Planting12 <simej1214@gmail.com>
2023-11-17 22:28:01 +0900

    🦄 refactor: 불필요한 공백 제거

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,7 +5,6 @@ import Timeline from "../components/timeline";
 const Wrapper = styled.div`
   display: grid;
   gap: 50px;
-
   grid-template-rows: 1fr 5fr;
   overflow-y: scroll;
   scrollbar-width: none;
