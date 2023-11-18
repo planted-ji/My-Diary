53d42bf4f3b02b201d0e2ea5f1521f72d55780de
Planting12 <simej1214@gmail.com>
2023-11-18 13:49:16 +0900

    🌈 style: 메뉴 아이콘에 대체 텍스트 추가

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -75,6 +75,7 @@ export default function Layout() {
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
+              aria-label="홈 화면으로 이동하기"
             >
               <path
                 clipRule="evenodd"
@@ -91,6 +92,7 @@ export default function Layout() {
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
+              aria-label="프로필 화면으로 이동하기"
             >
               <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
             </svg>
@@ -102,6 +104,7 @@ export default function Layout() {
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
             aria-hidden="true"
+            aria-label="로그아웃 하기"
           >
             <path
               clipRule="evenodd"
